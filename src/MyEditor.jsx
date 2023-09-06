93277de55d55a486d49d98cbb33a4815280c563c
dingo_jim <reach_jim@163.com>
2023-09-06 23:17:40 +0800

    微调布局

--- src/MyEditor.jsx
+++ src/MyEditor.jsx
@@ -4,11 +4,28 @@ import 'draft-js/dist/Draft.css';
 
 
 function MyEditor() {
-  const [editorState, setEditorState] = React.useState(
-    () => EditorState.createEmpty(),
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
   );
 
-  return <Editor editorState={editorState} onChange={setEditorState} />;
+  const editor = React.useRef(null);
+  function focusEditor() {
+    editor.current.focus();
+  }
+
+  return (
+    <div
+      style={{ minHeight: "6em", cursor: "text", margin:"0 0.5em" }}
+      onClick={focusEditor}
+    >
+      <Editor
+        ref={editor}
+        editorState={editorState}
+        onChange={setEditorState}
+        placeholder="Write something!"
+      />
+    </div>
+  );
 }
 
 export default MyEditor;
